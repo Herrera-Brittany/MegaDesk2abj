a4477fed99a964da047e1a254c500e6d838c398d
brittanyherrera <brittanyherrera@yahoo.com>
2018-02-14 22:14:23 -0600

    Add project files.

---
+++ MegaEscritorioDesktop/DeskQuoteView.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace MegaDesk2ABJ
+{
+    partial class DeskQuoteView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OrderDateLabel = new System.Windows.Forms.Label();
+            this.OrderDate = new System.Windows.Forms.Label();
+            this.DeskWidthLabel = new System.Windows.Forms.Label();
+            this.DeskDepthLabel = new System.Windows.Forms.Label();
+            this.DeskWidth = new System.Windows.Forms.Label();
+            this.DeskDepth = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.NumberOfDrawersLabel = new System.Windows.Forms.Label();
+            this.RushOrderLabel = new System.Windows.Forms.Label();
+            this.DesktopMaterialLabel = new System.Windows.Forms.Label();
+            this.QuoteAmountLabel = new System.Windows.Forms.Label();
+            this.Drawers = new System.Windows.Forms.Label();
+            this.Material = new System.Windows.Forms.Label();
+            this.RushDays = new System.Windows.Forms.Label();
+            this.QuoteTotal = new System.Windows.Forms.Label();
+            this.CustomerNameLabel = new System.Windows.Forms.Label();
+            this.CustomerName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OrderDateLabel
+            // 
+            this.OrderDateLabel.AutoSize = true;
+            this.OrderDateLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrderDateLabel.Location = new System.Drawing.Point(144, 87);
+            this.OrderDateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OrderDateLabel.Name = "OrderDateLabel";
+            this.OrderDateLabel.Size = new System.Drawing.Size(105, 22);
+            this.OrderDateLabel.TabIndex = 0;
+            this.OrderDateLabel.Text = "Quote Date:";
+            // 
+            // OrderDate
+            // 
+            this.OrderDate.AutoSize = true;
+            this.OrderDate.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrderDate.ForeColor = System.Drawing.Color.Blue;
+            this.OrderDate.Location = new System.Drawing.Point(296, 87);
+            this.OrderDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OrderDate.Name = "OrderDate";
+            this.OrderDate.Size = new System.Drawing.Size(98, 22);
+            this.OrderDate.TabIndex = 1;
+            this.OrderDate.Text = "placeholder";
+            // 
+            // DeskWidthLabel
+            // 
+            this.DeskWidthLabel.AutoSize = true;
+            this.DeskWidthLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeskWidthLabel.Location = new System.Drawing.Point(145, 151);
+            this.DeskWidthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DeskWidthLabel.Name = "DeskWidthLabel";
+            this.DeskWidthLabel.Size = new System.Drawing.Size(108, 22);
+            this.DeskWidthLabel.TabIndex = 2;
+            this.DeskWidthLabel.Text = "Desk Width:";
+            // 
+            // DeskDepthLabel
+            // 
+            this.DeskDepthLabel.AutoSize = true;
+            this.DeskDepthLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeskDepthLabel.Location = new System.Drawing.Point(145, 214);
+            this.DeskDepthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DeskDepthLabel.Name = "DeskDepthLabel";
+            this.DeskDepthLabel.Size = new System.Drawing.Size(106, 22);
+            this.DeskDepthLabel.TabIndex = 3;
+            this.DeskDepthLabel.Text = "Desk Depth:";
+            // 
+            // DeskWidth
+            // 
+            this.DeskWidth.AutoSize = true;
+            this.DeskWidth.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeskWidth.ForeColor = System.Drawing.Color.Blue;
+            this.DeskWidth.Location = new System.Drawing.Point(296, 151);
+            this.DeskWidth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DeskWidth.Name = "DeskWidth";
+            this.DeskWidth.Size = new System.Drawing.Size(98, 22);
+            this.DeskWidth.TabIndex = 4;
+            this.DeskWidth.Text = "placeholder";
+            // 
+            // DeskDepth
+            // 
+            this.DeskDepth.AutoSize = true;
+            this.DeskDepth.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeskDepth.ForeColor = System.Drawing.Color.Blue;
+            this.DeskDepth.Location = new System.Drawing.Point(296, 214);
+            this.DeskDepth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DeskDepth.Name = "DeskDepth";
+            this.DeskDepth.Size = new System.Drawing.Size(98, 22);
+            this.DeskDepth.TabIndex = 5;
+            this.DeskDepth.Text = "placeholder";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Red;
+            this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CloseButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.Location = new System.Drawing.Point(428, 540);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(84, 42);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.Text = "&Close";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // NumberOfDrawersLabel
+            // 
+            this.NumberOfDrawersLabel.AutoSize = true;
+            this.NumberOfDrawersLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumberOfDrawersLabel.Location = new System.Drawing.Point(63, 278);
+            this.NumberOfDrawersLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NumberOfDrawersLabel.Name = "NumberOfDrawersLabel";
+            this.NumberOfDrawersLabel.Size = new System.Drawing.Size(168, 22);
+            this.NumberOfDrawersLabel.TabIndex = 7;
+            this.NumberOfDrawersLabel.Text = "Number of Drawers:";
+            // 
+            // RushOrderLabel
+            // 
+            this.RushOrderLabel.AutoSize = true;
+            this.RushOrderLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RushOrderLabel.Location = new System.Drawing.Point(80, 405);
+            this.RushOrderLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.RushOrderLabel.Name = "RushOrderLabel";
+            this.RushOrderLabel.Size = new System.Drawing.Size(152, 22);
+            this.RushOrderLabel.TabIndex = 8;
+            this.RushOrderLabel.Text = "Rush Order (days):";
+            // 
+            // DesktopMaterialLabel
+            // 
+            this.DesktopMaterialLabel.AutoSize = true;
+            this.DesktopMaterialLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesktopMaterialLabel.Location = new System.Drawing.Point(84, 342);
+            this.DesktopMaterialLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DesktopMaterialLabel.Name = "DesktopMaterialLabel";
+            this.DesktopMaterialLabel.Size = new System.Drawing.Size(153, 22);
+            this.DesktopMaterialLabel.TabIndex = 9;
+            this.DesktopMaterialLabel.Text = "Desktop Material:";
+            // 
+            // QuoteAmountLabel
+            // 
+            this.QuoteAmountLabel.AutoSize = true;
+            this.QuoteAmountLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuoteAmountLabel.Location = new System.Drawing.Point(109, 469);
+            this.QuoteAmountLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.QuoteAmountLabel.Name = "QuoteAmountLabel";
+            this.QuoteAmountLabel.Size = new System.Drawing.Size(129, 22);
+            this.QuoteAmountLabel.TabIndex = 10;
+            this.QuoteAmountLabel.Text = "Quote Amount:";
+            // 
+            // Drawers
+            // 
+            this.Drawers.AutoSize = true;
+            this.Drawers.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Drawers.ForeColor = System.Drawing.Color.Blue;
+            this.Drawers.Location = new System.Drawing.Point(296, 278);
+            this.Drawers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Drawers.Name = "Drawers";
+            this.Drawers.Size = new System.Drawing.Size(98, 22);
+            this.Drawers.TabIndex = 12;
+            this.Drawers.Text = "placeholder";
+            // 
+            // Material
+            // 
+            this.Material.AutoSize = true;
+            this.Material.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Material.ForeColor = System.Drawing.Color.Blue;
+            this.Material.Location = new System.Drawing.Point(296, 342);
+            this.Material.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Material.Name = "Material";
+            this.Material.Size = new System.Drawing.Size(98, 22);
+            this.Material.TabIndex = 13;
+            this.Material.Text = "placeholder";
+            // 
+            // RushDays
+            // 
+            this.RushDays.AutoSize = true;
+            this.RushDays.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RushDays.ForeColor = System.Drawing.Color.Blue;
+            this.RushDays.Location = new System.Drawing.Point(296, 405);
+            this.RushDays.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.RushDays.Name = "RushDays";
+            this.RushDays.Size = new System.Drawing.Size(98, 22);
+            this.RushDays.TabIndex = 14;
+            this.RushDays.Text = "placeholder";
+            // 
+            // QuoteTotal
+            // 
+            this.QuoteTotal.AutoSize = true;
+            this.QuoteTotal.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuoteTotal.ForeColor = System.Drawing.Color.Blue;
+            this.QuoteTotal.Location = new System.Drawing.Point(296, 469);
+            this.QuoteTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.QuoteTotal.Name = "QuoteTotal";
+            this.QuoteTotal.Size = new System.Drawing.Size(98, 22);
+            this.QuoteTotal.TabIndex = 15;
+            this.QuoteTotal.Text = "placeholder";
+            // 
+            // CustomerNameLabel
+            // 
+            this.CustomerNameLabel.AutoSize = true;
+            this.CustomerNameLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerNameLabel.Location = new System.Drawing.Point(167, 32);
+            this.CustomerNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CustomerNameLabel.Name = "CustomerNameLabel";
+            this.CustomerNameLabel.Size = new System.Drawing.Size(88, 22);
+            this.CustomerNameLabel.TabIndex = 16;
+            this.CustomerNameLabel.Text = "Customer:";
+            // 
+            // CustomerName
+            // 
+            this.CustomerName.AutoSize = true;
+            this.CustomerName.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerName.ForeColor = System.Drawing.Color.Blue;
+            this.CustomerName.Location = new System.Drawing.Point(296, 32);
+            this.CustomerName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CustomerName.Name = "CustomerName";
+            this.CustomerName.Size = new System.Drawing.Size(98, 22);
+            this.CustomerName.TabIndex = 17;
+            this.CustomerName.Text = "placeholder";
+            // 
+            // DeskQuoteView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.CancelButton = this.CloseButton;
+            this.ClientSize = new System.Drawing.Size(537, 613);
+            this.ControlBox = false;
+            this.Controls.Add(this.CustomerName);
+            this.Controls.Add(this.CustomerNameLabel);
+            this.Controls.Add(this.QuoteTotal);
+            this.Controls.Add(this.RushDays);
+            this.Controls.Add(this.Material);
+            this.Controls.Add(this.Drawers);
+            this.Controls.Add(this.QuoteAmountLabel);
+            this.Controls.Add(this.DesktopMaterialLabel);
+            this.Controls.Add(this.RushOrderLabel);
+            this.Controls.Add(this.NumberOfDrawersLabel);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.DeskDepth);
+            this.Controls.Add(this.DeskWidth);
+            this.Controls.Add(this.DeskDepthLabel);
+            this.Controls.Add(this.DeskWidthLabel);
+            this.Controls.Add(this.OrderDate);
+            this.Controls.Add(this.OrderDateLabel);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DeskQuoteView";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mega Desk .:|:. Quote Details";
+            this.Load += new System.EventHandler(this.DeskQuoteView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OrderDateLabel;
+        private System.Windows.Forms.Label OrderDate;
+        private System.Windows.Forms.Label DeskWidthLabel;
+        private System.Windows.Forms.Label DeskDepthLabel;
+        private System.Windows.Forms.Label DeskWidth;
+        private System.Windows.Forms.Label DeskDepth;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label NumberOfDrawersLabel;
+        private System.Windows.Forms.Label RushOrderLabel;
+        private System.Windows.Forms.Label DesktopMaterialLabel;
+        private System.Windows.Forms.Label QuoteAmountLabel;
+        private System.Windows.Forms.Label Drawers;
+        private System.Windows.Forms.Label Material;
+        private System.Windows.Forms.Label RushDays;
+        private System.Windows.Forms.Label QuoteTotal;
+        private System.Windows.Forms.Label CustomerNameLabel;
+        private System.Windows.Forms.Label CustomerName;
+    }
+}
---
+++ MegaEscritorioDesktop/MainMenu.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MegaDesk2ABJ
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newQuoteButton = new System.Windows.Forms.Button();
+            this.viewQuotesButton = new System.Windows.Forms.Button();
+            this.searchQuotesButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // newQuoteButton
+            // 
+            this.newQuoteButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newQuoteButton.Location = new System.Drawing.Point(22, 16);
+            this.newQuoteButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.newQuoteButton.Name = "newQuoteButton";
+            this.newQuoteButton.Size = new System.Drawing.Size(169, 54);
+            this.newQuoteButton.TabIndex = 0;
+            this.newQuoteButton.Text = "&Add New Quote";
+            this.newQuoteButton.UseVisualStyleBackColor = true;
+            this.newQuoteButton.Click += new System.EventHandler(this.newQuoteButton_Click);
+            // 
+            // viewQuotesButton
+            // 
+            this.viewQuotesButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewQuotesButton.Location = new System.Drawing.Point(22, 76);
+            this.viewQuotesButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.viewQuotesButton.Name = "viewQuotesButton";
+            this.viewQuotesButton.Size = new System.Drawing.Size(169, 54);
+            this.viewQuotesButton.TabIndex = 1;
+            this.viewQuotesButton.Text = "&View Quotes";
+            this.viewQuotesButton.UseVisualStyleBackColor = true;
+            this.viewQuotesButton.Click += new System.EventHandler(this.viewQuotesButton_Click);
+            // 
+            // searchQuotesButton
+            // 
+            this.searchQuotesButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchQuotesButton.Location = new System.Drawing.Point(22, 135);
+            this.searchQuotesButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchQuotesButton.Name = "searchQuotesButton";
+            this.searchQuotesButton.Size = new System.Drawing.Size(169, 54);
+            this.searchQuotesButton.TabIndex = 2;
+            this.searchQuotesButton.Text = "&Search Quotes";
+            this.searchQuotesButton.UseVisualStyleBackColor = true;
+            this.searchQuotesButton.Click += new System.EventHandler(this.searchQuotesButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(184, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 135);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MegaDesk2ABJ.Properties.Resources.deskpic;
+            this.pictureBox2.Location = new System.Drawing.Point(236, 43);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 187);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ExitButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(22, 194);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(169, 54);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.appExit);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.CancelButton = this.ExitButton;
+            this.ClientSize = new System.Drawing.Size(415, 264);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.searchQuotesButton);
+            this.Controls.Add(this.viewQuotesButton);
+            this.Controls.Add(this.newQuoteButton);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainMenu";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".:|:. MegaDesk 2";
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newQuoteButton;
+        private System.Windows.Forms.Button viewQuotesButton;
+        private System.Windows.Forms.Button searchQuotesButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
